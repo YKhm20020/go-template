b9a63877680b6e4fbdcbc7ae0d6ee8f1c009be6b
YuyaKimura <molpui0726@gmail.com>
2025-04-06 15:47:34 +0900

    update: MySQL を用いてデータフェッチする機能とサンプルの処理を追加

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 - Go
 - Gin
+- Gorm
 - Air
 - MySQL
 - Docker
@@ -14,6 +15,7 @@
 
 1. Docker をインストール
 1. 本リポジトリをクローン、もしくは本リポジトリをテンプレートとしてリポジトリを作成
+1. .env ファイルを作成 (テンプレートは後述)
 1. `docker compose build` で Docker イメージをビルド
 
 # 必要なコマンド
@@ -24,18 +26,27 @@
 docker compose up
 ```
 
-サーバー起動後、http://localhost:8080 で動作を確認できる。services:
-app:
-build:
-context: .
-dockerfile: docker/go/Dockerfile
-depends_on: - db
-volumes: - ./src/app:/src/app
-ports: - "8080:8080"
-tty: true
+サーバー起動後、http://localhost:8080 で動作を確認できる。
 
 サーバー停止は、以下のコマンドを実行する。
 
 ```
 docker compose down
+```
+
+キャッシュなしでの再ビルドは、以下のコマンドを実行する。
+
+```
+docker compose build --no-cache
+```
+
+# .env ファイルテンプレート
+
+以下のように .env ファイルを作成するとよい。
+
+```
+DB_NAME=go-template-db
+DB_ROOT_PASSWORD=rootpassword
+DB_USER=db-user
+DB_PASSWORD=db-password
 ```
--- app/main.go
+++ app/main.go
@@ -1,18 +1,93 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Account struct {
+	ID       int
+	Name     string
+	Password string
+}
+
+func ConnectDB() (*gorm.DB, error) {
+	// データベース接続、.env ファイルが必要
+	// 詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(db)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Printf("データベース接続エラー: %v\n", err)
+		return nil, err
+	}
+
+	// マイグレーションの確認
+	err = db.AutoMigrate(&Account{})
+	if err != nil {
+		fmt.Printf("マイグレーションエラー: %v\n", err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
+	// データベース接続
+	db, err := ConnectDB()
+
+	if err != nil {
+		fmt.Printf("データベースとの接続に失敗\n")
+		return
+	}
+
+	// dsn := "root@tcp(mysql:3306)/first?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// dsn := "db-user:db-password@tcp(db:3306)/go-template-db?charset=utf8mb4&parseTime=True&loc=Local"
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, Gin + MySQL + Docker + Air",
 		})
+	})
+
+	// 全アカウント取得
+	r.GET("/accounts", func(c *gin.Context) {
+		var accounts []Account
+		result := db.Find(&accounts)
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": result.Error.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, accounts)
+	})
+
+	// IDによるアカウント取得
+	r.GET("/accounts/:id", func(c *gin.Context) {
+		var account Account
+		id := c.Param("id")
+		result := db.First(&account, id)
+		if result.Error != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "アカウントが見つかりません",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, account)
 	})
 
 	r.Run(":8080")
